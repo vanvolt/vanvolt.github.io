c5d53f741caf1642ead2d0acfbb3f461ef8e3ab2
vanvolt <36053988+vanvolt@users.noreply.github.com>
2018-02-14 14:17:47 -0600

    Feb. 13th work

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,34 +1,17 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
-
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(50);
 }
 
 function draw() {
-  background(255);
 
-  image(gear, 0, 0);
+  drawCircle(mouseX, mouseY);
 
-  stroke(0);
-  line(0, 0, 200, 200);
+}
 
-  fill(0, 255, 0, 100);
-  noStroke();
+function drawCircle (x, y) {
 
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+    noStroke();
+    fill(random(255), random(255), random(255), random(255));
+    ellipse(x, y, 100, 100)
 }
