4731fe3a2ee2179f3f164652b9ea8c222ed45fc2
vanvolt <36053988+vanvolt@users.noreply.github.com>
2018-03-21 14:17:59 -0600

    Learning more about arrays and noise function

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -14,11 +14,12 @@ function setup() {
 function draw() {
   background(255);
   displayTerrain();
+  plantFlag();
 }
 
 function generateInitialTerrain(numberOfRectangles) {
   let time = 0;
-  let dt = 0.005;
+  let dt = 0.006;
 
   for (let i=0; i<numberOfRectangles; i++) {
     let currentHeight = noise(time)*500;
@@ -31,11 +32,27 @@ function displayTerrain() {
   let rectWidth = width / numberOfRectangles;
   rectMode(CORNERS);
   fill(0);
+  stroke(0);
   for (let i=0; i<numberOfRectangles; i++) {
     rect(i*rectWidth, height, (i+1)*rectWidth, height - heights[i]);
   }
 }
 
 function plantFlag() {
+  let rectWidth = width / numberOfRectangles;
+  let highestX;
+  let tallest = 0;
+  for (let i=0; i<heights.length; i++) {
+    if (heights[i] > tallest) {
+      tallest = heights[i];
+      highestX = i*rectWidth;
+    }
+  }
 
+  let highestY = height - tallest;
+  stroke(255, 0, 0);
+  fill(255, 0, 0);
+  line(highestX, highestY, highestX, highestY - 50);
+
+  ellipse(highestX, highestY - 75, 50, 50);
 }
