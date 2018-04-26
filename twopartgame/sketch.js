55230e1dd559dc4c26d63cafbc1cd8ae55ef3216
vanvolt <36053988+vanvolt@users.noreply.github.com>
2018-04-26 15:13:39 -0600

    Renaming functions

--- twopartgame/sketch.js
+++ twopartgame/sketch.js
@@ -2,6 +2,7 @@
 let space;
 let troid, face, dizzy;
 let choice;
+let theTroids = [];
 
 function preload() {
   space = loadImage("images/background-space.jpg");
@@ -13,27 +14,57 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  choice = random(0,3);
+
 }
 
 function draw() {
   background(space);
-  if (choice <= 1) {
-    image(troid, mouseX - 100, mouseY - 80, 200, 160);
-  }
-  else if (choice >= 2) {
-    image(face,  mouseX - 50, mouseY - 50, 100, 100);
-  }
-  else if (choice > 1 && choice < 2){
-    image(dizzy,  mouseX - 48, mouseY - 48, 100, 100);
-  }
-
-
-
-  // fill(144, 0, 255);
-  // ellipse(mouseX, mouseY, 50, 50);
+  moveTroids();
+  displayTroids();
 }
 
-function mousePressed() {
+function moveTroids() {
+  for (let i=0; i<theTroids.length; i++) {
+    theTroids[i].x += theTroids[i].dx;
+    theTroids[i].y += theTroids[i].dy;
+  }
+}
+
+function displayTroids() {
   choice = random(0,3);
+  for (let i=0; i<theTroids.length; i++) {
+    if (theTroids[i].c <= 1) {
+      image(troid, theTroids[i].x - 100, theTroids[i].y - 80, 200, 160);
+    }
+    else if (theTroids[i].c >= 2) {
+      image(face, theTroids[i].x - 50, theTroids[i].y - 50, 100, 100);
+    }
+    else if (theTroids[i].c > 1 && theTroids[i].c < 2){
+      image(dizzy, theTroids[i].x - 50, theTroids[i].y - 50, 100, 100);
+    }
+  }
+}
+
+function mouseClicked() {
+  let xs;
+  let xl, yl;
+  let side = random(100);
+  if (side <= 50) {
+    xl = 0;
+    xs = random(3,7);
+  }
+  else if (side > 50) {
+    xl = width;
+    xs = random(-7, -3);
+  }
+  yl = random(50, height - 50);
+
+  let aBall = {
+    x: xl,
+    y: yl,
+    c: choice = random(0,3),
+    dx: xs,
+    dy: random(-2, 2),
+  };
+  theTroids.push(aBall);
 }
